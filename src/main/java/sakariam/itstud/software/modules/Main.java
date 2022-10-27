673bc80a29e5af5c8c9d91e9bc5ee9c7875dc39f
sakariam <111963152+sakariam@users.noreply.github.com>
2022-10-27 14:46:42 +0200

    Testing gradlew

--- src/main/java/sakariam/itstud/software/modules/Main.java
+++ src/main/java/sakariam/itstud/software/modules/Main.java
@@ -3,7 +3,7 @@ package sakariam.itstud.software.modules;
 public class Main {
     public static void main(String[] args)
     {
-        int year = 2000;
+        int year = 2200;
         if (leap_year.check_Leap_Year(year)==1)
             System.out.printf("Year %d is a leap year\n", year);
         else
